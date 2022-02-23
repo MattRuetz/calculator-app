fe9d6d016caedd9df7a71d1c3709e867f6bcc1d1
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-22 19:19:49 -0500

    full functionality implemented

--- script.js
+++ script.js
@@ -30,20 +30,35 @@ const addDecimal = () => {
     }
 };
 
+// Calculate operands with operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber, //result stored in 2nd operand
+};
+
 const useOperator = (operator) => {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }
 
     // Assign first value if undefined
     if (!firstValue) {
         firstValue = currentValue;
     } else {
-        console.log('current', currentValue);
+        console.log(firstValue, operatorValue, currentValue);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
     // Ready for next val... store operator
     awaitingNextValue = true;
     operatorValue = operator;
-    console.log('first:', firstValue);
-    console.log('op', operatorValue);
 };
 
 // Add event listener to ever button
